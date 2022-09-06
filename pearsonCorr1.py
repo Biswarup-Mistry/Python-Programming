2b574b79aed5ca65f65169b8fbbfc0cb591c4135
unknown <biswarupmistry3@gmail.com>
2022-09-07 02:31:13 +0530

    pearsonCorr1 is the correct one, others are with some minor defects

---
+++ pearsonCorr1.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from numpy import *
+import math
+
+
+# In[2]:
+
+
+data=pd.read_csv(r'E:\College_Notes\PG\Sem-3\DesignWork\DATA_sets\forestfires.csv')#'https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv')
+#data.head(10)
+#df=pd.DataFrame(data)
+data.drop(['month','day'],inplace=True, axis=1)
+data.head(5)
+
+
+# In[13]:
+
+
+i=0
+arr=[]
+for col in data:
+    arr.append(col)
+    i+=1
+i=0
+
+def pearson_corr(fi,fj):
+    x=data[fi]
+    y=data[fj]
+    k_x=[]
+    k_y=[]
+    k_x_sq=[]
+    k_y_sq=[]
+    l=[]
+    sum_x=np.sum(x)
+    sum_y=np.sum(y)
+    x_bar=sum_x/len(x)
+    y_bar=sum_y/len(y)
+    for i in x:
+        k_x.append(i-x_bar)
+    for i in y:
+        k_y.append(i-y_bar)
+    for i in k_x:
+        k_x_sq.append(i*i)
+    for i in k_y:
+        k_y_sq.append(i*i)
+    sum_k_x_sq=np.sum(k_x_sq)
+    std_dv_x=sum_k_x_sq/len(x)
+    sum_k_y_sq=np.sum(k_y_sq)
+    std_dv_y=sum_k_y_sq/len(y)
+    i=0
+    while i<len(x):
+        l.append(k_x[i]*k_y[i])
+        i+=1
+    sum_of_l=np.sum(l)
+    cov_x_y=sum_of_l/len(x)
+    #pears_corr=cov_x_y/((math.sqrt(std_dv_x))*(math.sqrt(std_dv_y)))
+    pears_corr=float("{:.2f}".format(cov_x_y/((math.sqrt(std_dv_x))*(math.sqrt(std_dv_y)))))
+    return pears_corr
+
+
+# In[14]:
+
+
+matrix=[]
+for row in data:
+    a=[]
+    for col in data:
+       a.append(pearson_corr(row,col))
+    matrix.append(a)
+
+#R=len(arr)
+#C=R
+#for i in range(R):
+#    for j in range(C): 
+#        print(matrix[i][j], end = " ")
+#    print() 
+
+
+# In[15]:
+
+
+#np.matrix=matrix
+#print(np.matrix)
+
+out_arr=np.vstack(matrix)
+print(out_arr)
+
+
+# In[16]:
+
+
+print(matrix[2][3])
+
+
+# In[17]:
+
+
+data.corr()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
